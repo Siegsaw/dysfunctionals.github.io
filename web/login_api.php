c1995a40ca6fe27a1cb6e8db83baf4bfdda92cc9
CaptainUnderpants-67 <bandonaxd@gmail.com>
2026-04-03 20:00:13 +0300

    Update login_api.php

--- web/login_api.php
+++ web/login_api.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
-require 'db.php';
+require '/var/www/private/db.php';
 session_start();
 
 $data = json_decode(file_get_contents('php://input'), true);
