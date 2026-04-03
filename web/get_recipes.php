0ac447213b4754250313b0b981af372a68744af8
CaptainUnderpants-67 <bandonaxd@gmail.com>
2026-04-03 22:36:04 +0300

    Refactor get_recipes.php to include additional fields

--- web/get_recipes.php
+++ web/get_recipes.php
@@ -2,18 +2,19 @@
 header('Content-Type: application/json');
 require '/var/www/private/db.php';
 
-// Get all recipes with their ingredients
 $sql = "
-    SELECT 
-        r.recipe_id,
-        r.name_recipe,
-        i.name_ing,
-        ri.quantity,
-        ri.unit
-    FROM recipes r
-    JOIN recipe_ingredients ri ON r.recipe_id = ri.recipe_id
-    JOIN ingredients i ON ri.ingredient_id = i.ingredient_id
-    ORDER BY r.recipe_id
+  SELECT 
+    r.recipe_id,
+    r.title,
+    r.total_time_minutes,
+    r.calories,
+    i.name_ing,
+    ri.quantity,
+    ri.unit
+  FROM recipes r
+  JOIN recipe_ingredients ri ON r.recipe_id = ri.recipe_id
+  JOIN ingredients i ON ri.ingredient_id = i.ingredient_id
+  ORDER BY r.recipe_id
 ";
 
 $result = $conn->query($sql);
@@ -21,25 +22,24 @@ $result = $conn->query($sql);
 $recipes = [];
 
 while ($row = $result->fetch_assoc()) {
-    $id = $row['recipe_id'];
+  $id = (int)$row['recipe_id'];
 
-    // Create recipe entry if not exists
-    if (!isset($recipes[$id])) {
-        $recipes[$id] = [
-            'id' => $id,
-            'name' => $row['name_recipe'],
-            'ingredients' => []
-        ];
-    }
-
-    // Add ingredient to recipe
-    $recipes[$id]['ingredients'][] = [
-        'name' => $row['name_ing'],
-        'amount' => (float)$row['quantity'],
-        'unit' => $row['unit']
+  if (!isset($recipes[$id])) {
+    $recipes[$id] = [
+      'id' => $id,
+      'name' => $row['title'],
+      'time' => isset($row['total_time_minutes']) ? (int)$row['total_time_minutes'] : null,
+      'calories' => isset($row['calories']) ? (float)$row['calories'] : null,
+      'ingredients' => []
     ];
+  }
+
+  $recipes[$id]['ingredients'][] = [
+    'name' => $row['name_ing'],
+    'amount' => (float)$row['quantity'],
+    'unit' => $row['unit']
+  ];
 }
 
-// Return as array (not object)
 echo json_encode(array_values($recipes));
 ?>
