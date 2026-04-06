036ec4a403bec92ffcdf82b4626ee1c7fbb760c1
SiegSaw <34284742+Siegsaw@users.noreply.github.com>
2026-04-06 21:18:08 +0300

    Create admin.php

---
+++ admin/admin.php
@@ -0,0 +1,14 @@
+<?php
+echo "<!DOCTYPE html>";
+echo "<html>";
+echo "";
+echo "<head>";
+echo "<title>First Web Page</title>";
+echo "</head>";
+echo "";
+echo "<body>";
+echo "Hello World!";
+echo "</body>";
+echo "";
+echo "</html>";
+?>
