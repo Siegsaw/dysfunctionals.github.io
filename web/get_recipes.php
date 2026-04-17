7c7583d08763088ca9ecb616ebdb9e6a4a89dece
Viktorija002 <vikkru1@ktu.lt>
2026-04-17 15:16:59 +0300

    Recipe filtering by flavor functionality get_recipes.php
    Scrum-91

--- web/get_recipes.php
+++ web/get_recipes.php
@@ -14,7 +14,8 @@ $sql = "
     i.name_ing,
     ri.quantity,
     ri.unit,
-    f.name AS flavor
+    GROUP_CONCAT(DISTINCT f.name SEPARATOR ',') AS flavors,
+    GROUP_CONCAT(DISTINCT CONCAT(i.name_ing, ':', ri.quantity, ':', ri.unit) SEPARATOR ';') AS ingredients_raw
 FROM recipes r
 JOIN recipe_ingredients ri ON r.recipe_id = ri.recipe_id
 JOIN ingredients i ON ri.ingredient_id = i.ingredient_id
@@ -28,6 +29,7 @@ LEFT JOIN (
     FROM recipe_steps
     GROUP BY recipe_id
     ) rs ON r.recipe_id = rs.recipe_id
+  GROUP BY recipe_id
   ORDER BY r.recipe_id
 ";
 
