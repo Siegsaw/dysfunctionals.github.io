7b97ad13fd30065b9275bb226a112b3da27e5631
Viktorija002 <vikkru1@ktu.lt>
2026-05-05 13:31:38 +0300

    Revert "Revert "Nutrition mapping""

--- admin/get_nutrition_mapping.php
+++ admin/get_nutrition_mapping.php
@@ -17,7 +17,7 @@ if ($ingredientId === '' || !is_numeric($ingredientId)) {
 }
 
 $stmt = $conn->prepare("
-    SELECT nutrient_id, amount_per_100g
+    SELECT nutrient_id, amount_per_100g, amount_per_unit
     FROM ingredient_nutrition
     WHERE ingredient_id = ?
 ");
@@ -30,7 +30,10 @@ $result = $stmt->get_result();
 $nutrition = [];
 
 while ($row = $result->fetch_assoc()) {
-    $nutrition[$row['nutrient_id']] = $row['amount_per_100g'];
+    $nutrition[$row['nutrient_id']] = [
+        'g100' => $row['amount_per_100g'],
+        'pcs'  => $row['amount_per_unit']
+    ];
 }
 
 echo json_encode([
