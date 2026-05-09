1f3e97fdbd29def53f6b84a545d32fc0978ea169
SiegSaw <34284742+Siegsaw@users.noreply.github.com>
2026-05-09 05:14:57 +0300

    Add link to Ingredients page in recipe admin

--- admin/add_recipe.php
+++ admin/add_recipe.php
@@ -21,6 +21,7 @@ echo "<a class='nav secondary' href='../web/index.php' target='_blank'>Main Webs
 echo "<a class='nav' href='manage_users.php'>User Manager</a>";
 echo "<a class='nav active' href='add_recipe.php'>Add Recipe</a>";
 echo "<a class='nav' href='modify_recipe.php'>Modify Recipes</a>";
+echo "<a class='nav' href='ingredients.php'>Ingredients</a>";
 echo "<a class='nav' href='add_nutrition.php'>Nutrition Mapping</a>";
 echo "<a class='nav secondary' href='logout.php'>Log out</a>";
 echo "</aside>";
